68f71269adab173f1597802739878d2c184af503
akshat Garg <gargaks@sheridancollege.ca>
2024-01-17 10:03:18 -0500

    Revert ""
    This reverts commit 784efa43e5654fce335aa2cdccfabc9741373345.

--- src/arraysdemo/StudentList.java
+++ src/arraysdemo/StudentList.java
@@ -9,7 +9,6 @@ public class StudentList {
     public static void main(String[] args){
     
         Student s1 = new Student(11,"Akshat");
-        System.out.println("hello");
         Student[] studentList = new Student[3];
         studentList[0] = s1;
         studentList[1] = new Student(2,"abc");
